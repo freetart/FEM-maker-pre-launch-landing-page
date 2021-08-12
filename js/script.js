b3defce482a9fd4b960383c58f396a16d232ea26
habib <habibdev96@gmail.com>
2021-08-12 02:30:43 -0600

    created email validation for cta

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import preloader from "./preloader.js";
+import validateEmail from "./validateEmail.js";
 
 const init = () => {
   preloader();
+  validateEmail();
 };
 
 window.addEventListener("DOMContentLoaded", init);
---
+++ js/validateEmail.js
@@ -0,0 +1,40 @@
+const validateEmail = () => {
+  const form = document.querySelector(".cta__form");
+  const emailInput = document.querySelector(".cta__input");
+
+  // check input
+  const checkInput = () => {
+    const emailValue = emailInput.value.trim();
+
+    if (emailValue === "") {
+      setErrorMsg(emailInput, "Email cannot be blank");
+    } else if (!isEmail(emailValue)) {
+      setErrorMsg(emailInput, "Email is not valid");
+    }
+  };
+
+  // error message
+  const setErrorMsg = (input, message) => {
+    const inputGroup = input.parentElement;
+    const errorMessage = inputGroup.querySelector(".cta__message");
+    errorMessage.innerText = message;
+    inputGroup.className = "cta__form error";
+    emailInput.style.border = "0.1rem solid var(--red)";
+  };
+
+  // check if valid email
+  const isEmail = (email) => {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  };
+
+  // submit form
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInput();
+  });
+};
+
+export default validateEmail;
